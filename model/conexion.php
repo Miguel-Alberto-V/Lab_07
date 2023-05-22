2bc16a29e1505b16fddfd9a355c1a349acddfe52
Miguel-Alberto-V <miguelangelalberto2004@gmail.com>
2023-05-22 15:45:16 -0500

    Modificacion

--- model/conexion.php
+++ model/conexion.php
@@ -1,14 +1,14 @@
 <?php 
-$contrasena = "";
-$usuario = "root";
-$nombre_bd = "lab06";
+$contrasena = "AVNS_koSn5tw88NQbySRImkJ";
+$usuario = "doadmin";
+$nombre_bd = "railway";
+$host = "db-mysql-nyc1-60133-do-user-14116195-0.b.db.ondigitalocean.com:25060";
 
 try {
 	$bd = new PDO (
-		'mysql:host=localhost;
-		dbname='.$nombre_bd,
-		$usuario,
-		$contrasena,
+		"mysql:host={$host};dbname={$nombre_bd}",
+        $usuario,
+        $contrasena,
 		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
 	);
 } catch (Exception $e) {
